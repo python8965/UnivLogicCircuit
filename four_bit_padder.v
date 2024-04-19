e2dcafdb1ca74b5a9e2a26950443e096658e0ca6
aketon <python8965@gmail.com>
2024-04-19 14:50:45 +0900

    save

---
+++ four_bit_padder.v
@@ -0,0 +1,29 @@
+module four_bit_padder(A, B, S, Cout, V,p );
+
+input [3:0] A;
+input [3:0] B;
+output [3:0] S;
+output Cout, V, p;
+
+wire c1, c2, c3;
+
+full_adder F1 (A[0], B[0], 0, S[0], c1);
+full_adder F2 (A[1], B[1], c1, S[1], c2);
+full_adder F3 (A[2], B[2], c2, S[2], c3);
+full_adder F4 (A[3], B[3], c3, S[3], Cout);
+
+wire na3, nb3, ns3;
+
+not (na3, A[3]);
+not (nb3, B[3]);
+not (ns3, S[3]);
+wire v1, v2;
+
+
+and Vand1 (v1, na3, nb3, S[3]);
+and Vand2 (v2, A[3], B[3], ns3);
+
+or Vor (V, v1, v2);
+
+assign p = 1'b0;
+endmodule
--- full_adder.v
+++ full_adder.v
@@ -1,9 +1,7 @@
-module full_adder(x, y, cin, sum, cout, p);
+module full_adder(x, y, cin, sum, cout);
 input x, y, cin;
-output sum, cout, p;
+output sum, cout;
 wire w1, w2, w3;
-
-assign p = 1'b0;
 
 half_adder HA1(x, y, w1, w2);
 half_adder HA2(w1, cin, sum, w3);
